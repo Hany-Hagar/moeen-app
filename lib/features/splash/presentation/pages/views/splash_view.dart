b3a91963c354c5b206fc84d8cdee8d020b3a05a4
HanyHagar <hanymagdyhagar@gmail.com>
2026-01-26 21:23:40 +0200

    feat(splash): add splash view logic

--- lib/features/splash/presentation/pages/views/splash_view.dart
+++ lib/features/splash/presentation/pages/views/splash_view.dart
@@ -1,12 +1,23 @@
 import '../widgets/splash_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../../../core/settings/presentation/manager/settings_cubit.dart';
+import '../../../../../core/settings/presentation/manager/settings_state.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Stack(children: const [_BackGound(), SplashBody()]));
+    return BlocListener<SettingsCubit, SettingsState>(
+      listener: (context, state) {
+        if (state is SettingsLoaded) {}
+      },
+
+      child: Scaffold(
+        body: Stack(children: const [_BackGound(), SplashBody()]),
+      ),
+    );
   }
 }
 
